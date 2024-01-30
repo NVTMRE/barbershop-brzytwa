23987abc483e3aaf2a13ed70d13a4146ed23e70f
H3xWizz <ksawier.malkiewicz@icloud.com>
2024-01-30 14:52:04 +0100

    fix navbar again

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,7 +8,7 @@ import {
     NavigationMenuList,
 } from "@/components/ui/navigation-menu"
 import Image from "next/image"
-import {Button, buttonVariants} from "./ui/button";
+import {buttonVariants} from "./ui/button";
 import {useEffect, useState} from "react";
 import {cn} from "@/lib/utils";
 import {NavItems} from "@/module/nav-items";
@@ -17,7 +17,7 @@ export default function Navbar() {
     const [isScrolled, setIsScrolled] = useState(false)
     useEffect(() => {
         const handleScroll = () => {
-            setIsScrolled(window.scrollY > 20 ? true : false);
+            setIsScrolled(window.scrollY > 20);
         };
         handleScroll();
         window.addEventListener("scroll", handleScroll);
@@ -29,9 +29,9 @@ export default function Navbar() {
     return (
         <div className={cn(
             isScrolled ? 'bg-background text-foreground' : 'bg-transparent text-white',
-            'fixed top-0 py-2 px-8 flex w-full items-center transition-colors gap-x-4'
+            'z-10  fixed top-0 py-2 px-8 w-full flex justify-between items-center transition-colors gap-x-4'
         )}>
-            <div className={'flex absolute gap-3 transition-colors'}>
+            <div className={'flex gap-3 transition-colors'}>
                 <Link href={'https://www.facebook.com/BrzytwaBarbershop/'}>
                     <FaFacebook size={23}/>
                 </Link>
@@ -39,56 +39,22 @@ export default function Navbar() {
                     <FaInstagram size={23}/>
                 </Link>
             </div>
-            <NavigationMenu className="flex flex-1 items-center justify-end max-w-full">
-                <NavigationMenuList className={'flex items-center justify-center gap-3'}>
-                    <NavigationMenuItem>
-                        <Link href={'#About'}>
-                            <p className={'hover:text-black transition-colors'}>O nas</p>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Link href={'#Offer'}>
-                            <p className={'hover:text-black transition-colors'}>Oferta</p>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Link href={'#Team'}>
-                            <p className={'hover:text-black transition-colors'}>Zespół</p>
-                        </Link>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
             <NavigationMenu>
-                <NavigationMenuList className={'flex items-center justify-center gap-3'}>
-                    <NavigationMenuItem>
-                        <Link href={'#Hero'}>
-                            <Image src={'/logo.png'} alt={"Logo"} width={65} height={65}/>
-                        </Link>
-                    </NavigationMenuItem>
+                <NavigationMenuList className="flex items-center gap-2">
+                    {NavItems.map((item, index) => (
+                        <NavigationMenuItem key={index}>
+                            <Link href={item.href}>
+                                {item.img ? <Image src={item.img} alt={item.name} width={75} height={75} className={'mx-4'}/> : <p>{item.name}</p>}
+                            </Link>
+                        </NavigationMenuItem>
+                    ))}
                 </NavigationMenuList>
             </NavigationMenu>
-            <NavigationMenu className="flex flex-1 items-center justify-start max-w-full">
-                <NavigationMenuList className={'flex items-center justify-center gap-3'}>
-                    <NavigationMenuItem>
-                        <Link href={'#Training'}>
-                            <p className={'hover:text-black transition-colors'}>Szkolenia</p>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Link href={'#Gallery'}>
-                            <p className={'hover:text-black transition-colors'}>Galeria</p>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Link href={'#Contact'}>
-                            <p className={'hover:text-black transition-colors'}>Kontakt</p>
-                        </Link>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-            <Link href={'https://barbershopbrzytwa.booksy.com'} className={cn(buttonVariants({variant: 'outline'}), 'right-5 border-black absolute bg-transparent border-[0.15rem] rounded-radius h-8 px-3 text-[0.75rem] text-black')}>
-                <p>UMÓW SIĘ</p>
-            </Link>
+            <div>
+                <Link href={'https://barbershopbrzytwa.booksy.com'} className={cn(buttonVariants({variant: 'outline'}), 'absolute right-6 top-1/3 border-black bg-transparent border-[0.15rem] rounded-radius h-8 px-3 text-[0.75rem] text-black')}>
+                    <p>UMÓW SIĘ</p>
+                </Link>
+            </div>
         </div>
     )
 }
